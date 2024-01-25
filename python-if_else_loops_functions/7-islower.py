e0b0f9120b734a32516785e3105391c742ea8da3
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-25 10:51:54 -0800

    add file

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,13 +2,13 @@
 
 def islower(c):
     is_lower = False
-    
-    if not isinstance(c, str):
-        return False
+
     for d in c:
-        if 'A' <= d <= 'Z':
-            return False
-        elif 'a' <= d <= 'z':
+        if 'a' <= d <= 'z':
             is_lower = True
+        elif 'A' <= d <= 'Z':
+            return False
+        else:
+            continue
 
     return is_lower
