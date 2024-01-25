72846e1befcd14b422cdbe4b307dd3fdd12e6771
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-25 13:43:50 -0800

    task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
-            print("{}".format("Fizz"), end=" ")
+            print("{}".format("Fizz"), end="")
         if i % 5 == 0:
-            print("{}".format("Buzz"), end=" ")
-        
+            print("{}".format("Buzz"), end="")
+
         if i % 3 != 0 and i % 5 != 0:
-            print("{}".format(i), end=" ")
+            print("{}".format(i), end="")
+        print("", end=" ")
