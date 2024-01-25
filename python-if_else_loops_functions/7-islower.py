39cc4730fb70595e4d2a8f0da892f7607f30f925
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-25 11:04:59 -0800

    add file

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
+
 def islower(c):
-    is_lower = False
-
-    for d in c:
-        if 'a' <= d <= 'z':
-            is_lower = True
-        elif 'A' <= d <= 'Z':
-            return False
-        else:
-            continue
-
-    return is_lower
+    return 97 <= ord(c) <= 122
