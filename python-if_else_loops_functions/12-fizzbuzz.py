341f5c5ac1dcc2497087379d6dc1962cf4e9a1e2
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-25 12:53:01 -0800

    task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("{}".format("Fizz"), end="")
+        if i % 5 == 0:
+            print("{}".format("Buzz"), end="")
+        
+        if i % 3 != 0 and i % 5 != 0:
+            print("{}".format(i), end="")
+
+        if i != 100:
+            print(" ", end="")
