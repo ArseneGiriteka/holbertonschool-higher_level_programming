e5d9a101d407042beaaa9bebdfa26971d52aeca9
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-25 08:29:36 -0800

    task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def islower(c):
     is_lower = False
+    if len(c) > 1:
+        return False
     for d in c:
         if 'A' <= d and d <= 'Z':
             return False
-        elif ('a' <= d and 'z' >= d) or d == ' ':
+        elif 'a' <= d and 'z' >= d:
             is_lower = True
     return is_lower
