6d23ff34b239660a05108b3b16a3d313672e5a3a
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-25 17:33:00 -0800

    task 4

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def main():
+    names = dir(hidden_4)
+    unsorted_names = []
+    sorted_names = []
+    for name in names:
+        if not name.startswith("__"):
+            unsorted_names.append(name)
+
+    sorted_names = sorted(unsorted_names)
+    for item in sorted_names:
+        print(name)
+
+
+if __name__ == "__main__":
+    main()
