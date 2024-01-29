f86e8ad94de3a95f79fdb00817ab5efeea32430b
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-29 01:45:18 -0800

    adding files

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for item in my_list:
-        print("{}".format(item))
+    for index in range(0, len(my_list)):
+        print("{}".format(my_list[index]))
