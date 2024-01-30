6899814feaad19030f614ebce0b30ca285847cec
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-30 10:39:47 -0800

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@
 def best_score(a_dictionary):
     max_score = 0
     max_key = ''
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary.values()) == 0:
         return None
     for item in a_dictionary.items():
         if item[1] > max_score:
