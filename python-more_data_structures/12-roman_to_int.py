29902e64d77fab51c4f61bae085286d1d40c1a06
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-30 15:04:26 -0800

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,23 +3,27 @@
 
 def roman_to_int(roman_string):
     result = 0
+    i = 0
     _str = roman_string
 
     if roman_string is None or not isinstance(roman_string, str):
         return 0
 
-    for i in range(0, len(_str)):
+    while i < len(_str):
         symbol_1 = get_roman_value(_str[i])
 
-        if i + 1 < len(_str[i]):
+        if i + 1 < len(_str):
             symbol_2 = get_roman_value(_str[i + 1])
 
             if symbol_2 <= symbol_1:
-                result += symbol_2 + symbol_1
+                result += symbol_1
+                i += 1
             else:
                 result += symbol_2 - symbol_1
+                i += 2
         else:
             result += symbol_1
+            i += 1
     return result
 
 
@@ -39,4 +43,4 @@ def get_roman_value(roman_string):
     elif roman_string == 'I':
         return 1
     else:
-        return 0
+        return -1
