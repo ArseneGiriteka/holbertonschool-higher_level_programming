3a9d24ff5b4f25ec6d482f4eaae426f606f7b4d3
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-30 04:34:43 -0800

    task 6

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    list(map(
+        lambda key: print("{} : {}".format(key, a_dictionary[str(key)])),
+        sorted(a_dictionary.keys())
+        )
+        )
