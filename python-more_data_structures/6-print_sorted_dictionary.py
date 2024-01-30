3013c358f4f17124f993747614662a16c697b8a6
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-30 04:48:26 -0800

    task 6 fixing

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,7 +3,7 @@
 
 def print_sorted_dictionary(a_dictionary):
     list(map(
-        lambda key: print("{} : {}".format(key, a_dictionary[key])),
+        lambda key: print("{}: {}".format(key, a_dictionary[key])),
         sorted(a_dictionary.keys())
         )
         )
