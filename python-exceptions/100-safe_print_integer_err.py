e3df005552c4f89a5e74b7d818c5a37be1de0416
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-31 02:56:49 -0800

    task 100

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
