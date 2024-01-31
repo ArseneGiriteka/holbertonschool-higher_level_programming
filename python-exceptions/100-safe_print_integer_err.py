33e41ed7534ec24c2a6bde72d5feb7034f4aca61
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-31 03:04:30 -0800

    task 100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as ex:
-        print(ex)
+        print("Exception: {}".format(ex))
         return False
