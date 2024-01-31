4a0fffbe0e0f299e2bf5771048a9e647f6d6b665
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-31 01:14:58 -0800

    task 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    res = None
+
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(res))
+        return res
