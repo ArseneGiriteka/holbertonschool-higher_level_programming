f760970a32208413f1894e6ea83b8e8d948cf996
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-31 02:00:49 -0800

    task 100

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -4,6 +4,8 @@
 def weight_average(my_list=[]):
     res = 0
     w_sum = 0
+    if not my_list:
+        return 0
     for item in my_list:
         res += item[0] * item[1]
         w_sum += item[1]
