d080309c47f94d58e5faf32266f438ea848715f8
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-02-06 10:20:51 -0800

    task 1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,8 +5,3 @@ class Square:
     """
     This class will represente the square object
     """
-    def __init__(self):
-        """
-        This a method to initialize the square instance
-        """
-        pass
