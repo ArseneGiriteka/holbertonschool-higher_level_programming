f25075f1f6621a5734b5feca08b7436359fab084
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-02-06 10:56:48 -0800

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module define a class called Square
+Attributes:
+    size (int): it represente the size of a side of
+    the square object
+
+Todo:
+    define class Square
+    define a private instance attribute calle "size"
+    raise TypeError if size isn't integer
+    raise ValueError if size is less that zero
+"""
+
+
+class Square:
+    """
+    Note:
+        This class represente a Square object
+
+    Attributes:
+        size (int) : private instance attribute
+
+    Args:
+        size (int)
+    """
+    def __init__(self, size=0):
+        """
+        Note:
+            This methode initialize a square object
+            It raises TypeError if size is not integer
+            It raises ValueError if size < 0
+
+        Args:
+            self (Square): private square instance object
+            size (int): square object one side size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
