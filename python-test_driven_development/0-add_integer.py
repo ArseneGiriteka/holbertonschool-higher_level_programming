0b73e490b89a589f9d732d8516b7df494dc13a35
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-02-08 14:27:35 +0100

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,9 +10,9 @@ def add_integer(a, b=98):
     """
     This function add two integers
     """
-    if a is None or not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif b is None or not isinstance(b, (int, float)):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     else:
         return int(a) + int(b)
