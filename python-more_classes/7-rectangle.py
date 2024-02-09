ec9d9e5c034188db57d74a04281ba8cee738f9b6
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-02-09 21:24:07 +0100

    task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -67,7 +67,8 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ""
 
-        return "\n".join([str(self.print_symbol) * self.__width] * self.__height)
+        return "\n".join(
+            [str(self.print_symbol) * self.__width] * self.__height)
 
     def __repr__(self):
         """__repr__ function"""
