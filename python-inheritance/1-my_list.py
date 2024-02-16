ba8f31ad989ad10b363ea46232beafca4d533af0
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-02-16 12:14:19 +0100

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is a module"""
+
+
+class MyList(list):
+    """This is MyList class"""
+
+
+    def print_sorted(self):
+        """Print_sorted method"""
+        print(sorted(self))
