8bc8a347a624c2428feca6ddce381ef93f225f03
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-02-16 14:07:30 +0100

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+def inherits_from(obj, a_class):
+    """
+    Check if obj is an instance of a class
+    that inherited (directly or indirectly)
+    from the specified class a_class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        True if obj is an instance of a subclass of a_class, False otherwise.
+    """
+    if isinstance(obj, a_class) and not type(obj) == a_class:
+        return True
+    return False
