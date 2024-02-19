cfff933c94b2ce8558c1785bdf60b1d05892ae7a
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-02-19 12:11:24 +0100

    task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,5 +6,8 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    this is a method
+    """
     with open(filename, mode="w", encoding="utf-8") as file:
         json.dump(my_obj, file)
