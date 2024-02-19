1521e14a1457bcdb2261e49362301798af586b22
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-02-19 10:22:56 +0100

    fixing 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,6 +11,6 @@ def inherits_from(obj, a_class):
     Returns:
         True if obj is an instance of a subclass of a_class, False otherwise.
     """
-    if isinstance(obj, a_class) and not type(obj) == a_class:
+    if isinstance(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
