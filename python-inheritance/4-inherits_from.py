cce7e02af188b3ce01e02450abdc9b72247005f6
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-02-19 10:34:24 +0100

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-This a module
-
-this module check if an object inherits from some thye of an object
+Note:
+    this module check if an object inherits from some thye of an object
 
 Methods:
-    def inherits_from(obj, a_class):
+    def inherits_from(obj, a_class)
 """
 
 
 def inherits_from(obj, a_class):
     """
-    Check if obj is an instance of a class
-    that inherited (directly or indirectly)
-    from the specified class a_class.
+    Note:
+        Check if obj is an instance of a class
+        that inherited (directly or indirectly)
+        from the specified class a_class.
 
     Args:
         obj: The object to check.
