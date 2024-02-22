221b3f5956a656d7d6480ebc818f723603d88e42
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-02-22 13:00:56 +0100

    fixing doc

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         """this is a method"""
-        super.__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
